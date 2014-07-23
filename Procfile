a077841893b4f29149e769394cbd92eba0ae5de0
Steve Winton <steve.winton@gmail.com>
2014-07-23 11:52:42 -0500

    Config for Heroku deployment.

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn -w 4 --pythonpath ./web/slickshot slickshot.wsgi --log-file -
--- web/slickshot/slickshot/register.py
+++ web/slickshot/slickshot/register.py
@@ -9,6 +9,20 @@ def job(job, selections):
     """
     Registers a new job in the platform (generates CSV and updates index).
     """
+
+    # TODO
+    # Generate CSV and upload directly to Dropbox using the REST API
+    # Be sure to use overwrite mode.
+
+    # TODO
+    # Append job to Redis 'jobs' list
+    # Just set it as the last element so it's as quick as possible (O(1))
+
+    # TODO
+    # Generate new index.txt from Redis 'jobs' list
+    # Upload to Dropbox using the REST API
+    # Be sure to use overwrite mode.
+
     # Generate CSV
     with open(os.path.join(settings.PLATFORM_HOME, "jobs", job, "{}.csv".format(job)), "wb") as fp:
         writer = csv.writer(fp)
